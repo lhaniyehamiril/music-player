506200d1495dc01aea80a0b6fe0b23b7b11ceb49
haniyeh amiri <amirihaniyhe@gmail.com>
2026-05-30 23:11:41 +0330

    improve ui

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -8,13 +8,13 @@ type songListProps = {
 const SongList = ({findArtist} : songListProps) => {
  
   return (
-    <div className="flex items-center justify-center">
+    <div className="md:flex md:items-center md:justify-center">
     <div className="flex items-center md:w-80 flex-col justify-center mt-6 gap-5">
       {findArtist?.songs.map((item,i) => 
         <Link to={`/artist/${findArtist.id}/song/${item.id}`} key={i}  className="bg-white w-[95%] gap-1 h-13 flex items-center rounded-full relative">
            
           <img src={item.img} className="w-14 h-14 p-2 object-cover rounded-full" />
-          <span className="text-black font-semibold">{item.title}</span>
+          <span className="text-black font-semibold text-[14px] md:text-[15px]">{item.title}</span>
 
           <HiPlay color="#000" size={35} className="absolute right-2"/>
         </Link>
